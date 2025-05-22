a5de77f70cc25930daec59568fb6fe6971db0db9
Chloe Lanthier <chloedino06@gmail.com>
2025-05-22 14:06:01 -0400

    hey

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,40 +1,29 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MusicManager : MonoBehaviour
 {
     private static MusicManager instance;
     private AudioSource audioSource;
+    [SerializeField] private AudioMixer audioMixer;
 
     void Awake()
-{
-    if (instance == null)
     {
-        instance = this;
-        DontDestroyOnLoad(gameObject);
-        audioSource = GetComponent<AudioSource>();
-
-        float volume = PlayerPrefs.GetFloat("musicVol", 0.5f);
-
-        audioSource.volume = volume;
-
-        audioSource.loop = true;
-        audioSource.Play();
-    }
-    else
-    {
-        Destroy(gameObject);
-    }
-}
-
-
-    public void UpdateVolume(float newVolume)
-    {
-        AudioListener.volume = newVolume;
-        PlayerPrefs.SetFloat("volume", newVolume);
-
-        if (audioSource != null)
+        if (instance == null)
         {
-            audioSource.volume = newVolume;
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            audioSource = GetComponent<AudioSource>();
+
+            float volume = PlayerPrefs.GetFloat("musicVol", 0.5f);
+            audioMixer.SetFloat("MusicVolume", Mathf.Log10(Mathf.Clamp(volume, 0.0001f, 1)) * 20);
+
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 }
